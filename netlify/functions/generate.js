ef78479aa3bd711156cc70b124aa38fb5731f7d5
kafr-rol <kafr.rol@gmail.com>
2026-02-10 14:33:29 +0200

    Update generate.js

--- netlify/functions/generate.js
+++ netlify/functions/generate.js
@@ -3,7 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 exports.handler = async (event) => {
   try {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
     const prompt = `
       Сгенерируй для мема "цель ТЦК":
@@ -32,4 +32,5 @@ exports.handler = async (event) => {
       body: JSON.stringify({ error: error.message })
     };
   }
+
 };
